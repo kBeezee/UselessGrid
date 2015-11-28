fe3376f15cdec431a56554644e4b13ceeae15a11
Kile <kboxer4513@gmail.com>
2015-11-27 20:10:47 -0600

    .

--- mapping.py
+++ mapping.py
@@ -1,4 +1,5 @@
 #Handle Mapping
+from globals import *
 def Gen_SmallGrid(fbig_Grid, fTOP_LEFT):
     # GLOBALS
     # Do Not use globals in seperate module, just update 1 variable at a time via return
@@ -6,14 +7,35 @@ def Gen_SmallGrid(fbig_Grid, fTOP_LEFT):
 
     # init 10x10, empty grid.
     Grid = []
-    for row in range(20):
+    for row in range(g_HEIGHT):
         Grid.append([])
-        for column in range(20):
+        for column in range(g_WIDTH):
             pass
 
     # fill that empty grid with what is in the big grid, based on the offset.
-    for row in range(20):
-        for column in range(20):
+    for row in range(g_HEIGHT):
+        for column in range(g_WIDTH):
             Grid[row].append(fbig_Grid[fTOP_LEFT[0]+row][fTOP_LEFT[1]+column])
     # return the new grid to the calling function.
     return Grid
+
+
+def GridOutOfBounds(fTOP_LEFT, dir):
+    # Move the grid up and down
+    if dir == "up":
+        if fTOP_LEFT[0] >= 0 and fTOP_LEFT[0] <= MAX_HEIGHT - g_HEIGHT:
+            return True
+        else:
+            print "Cant Move, u/d"
+            return False
+
+    # Move the grid left and right
+    if dir == "left":
+        if fTOP_LEFT[1] >= 0 and fTOP_LEFT[1] <= MAX_WIDTH - g_WIDTH:
+            return True
+        else:
+            print "Cant Move, l/r"
+            return False
+
+
+
