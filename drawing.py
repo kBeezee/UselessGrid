32ca86a3012ebb2a6b33e1af12f36e1719d27850
Kile <kboxer4513@gmail.com>
2015-12-02 23:07:03 -0600

    TODO: Get the Sprites to snap to the Isometric grid, being sure to be able to get the cordinates of the grid it rests on.

--- drawing.py
+++ drawing.py
@@ -6,7 +6,7 @@ import maps
 
 #container tiles which are surfaces
 tileset = []
-#dictonary
+dicScale = {'sprite': (32, 32), 'tile': (64, 64)}
 
 """"
 Really this isnt drawing anything anymore, its preparing everything to be drawn.
@@ -20,7 +20,7 @@ For   tiles its getting them from a file, adding the *surfaces* to a regular lis
 #this is one place holder for the player.
 fftSpirteSheet = libSpriteSheet.spritesheet("res/fftFemaleWhiteMage.png") #ColorKey: Black
 fftSpirteSheet.ColorKey = (255, 255, 255)
-myObjects.Player(fftSpirteSheet.images_at(fftWalkingLoop, "sprite",  colorkey=fftSpirteSheet.ColorKey), 416, 160, "p1")
+myObjects.Player(fftSpirteSheet.images_at(fftWalkingLoop, dicScale['sprite'],  colorkey=fftSpirteSheet.ColorKey), 416, 160, "p1")
 
 #
 #fftSpirteSheet = libSpriteSheet.spritesheet("res/fftFemaleBlackMage.png") #ColorKey: Black
@@ -40,14 +40,14 @@ blocks=[pygame.Rect(0, 0, 128, 128), pygame.Rect(128, 0, 128, 128), pygame.Rect(
         pygame.Rect(768, 0, 128, 128), pygame.Rect(896, 0, 128, 128)]
 
 #turn those tiles into a list of surfaces
-tileset += TilesFileOne.images_at(blocks, "tile", colorkey=TilesFileOne.ColorKey)
+tileset += TilesFileOne.images_at(blocks, dicScale['tile'], colorkey=TilesFileOne.ColorKey)
 
 #get another image with tiles/sprites
 TilesFileTwo = libSpriteSheet.spritesheet("res/basic_water_tiles-black.png")
 TilesFileTwo.ColorKey = (0, 0, 0)
 
 #get a list of surfaces
-tileset += TilesFileTwo.images_at(blocks, "tile", colorkey=(0, 0, 0))
+tileset += TilesFileTwo.images_at(blocks, dicScale['tile'], colorkey=(0, 0, 0))
 
 #pass tilesets to be used to make the visible map.
 maps.MakeMap0(tileset)
